d1c77336b8043fa8c832f3ba2443a0298b51f82c
Gustav W Delius <gustav.delius@gmail.com>
2016-11-23 11:14:26 +0000

    Removed loop. Fixed some indenting.

--- R/Program1.R
+++ R/Program1.R
@@ -10,15 +10,11 @@ DerivativeMatrixError <- function(N){
   uprime <- cos(x)*u;
   D <- sparseMatrix((1:N), c((2:N),1), x = 2*rep(1, N)/3)-sparseMatrix((1:N), c((3:N),1,2), x = rep(1, N)/12)
   D <- (D-t(D))/h;
-return(max(abs(D %*% u -uprime))
-)  
+  return(max(abs(D %*% u -uprime)))  
 }
 
 
 Nvec=2^(3:12);
-errorvalues <- rep(0,length(Nvec));
-for (i in 1:(length(errorvalues))) {
- errorvalues[i] <- DerivativeMatrixError(Nvec[i])
-}
+errorvalues <- sapply(Nvec, DerivativeMatrixError)
 
 plot(Nvec,errorvalues, log="xy")
